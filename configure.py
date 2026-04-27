eec7f7ff47c8188d83a9a3bb8931815ec3482364
Cuyler36 <Cuyler36@users.noreply.github.com>
2026-04-27 18:48:21 -0400

    Link m_field_info

--- configure.py
+++ configure.py
@@ -971,7 +971,7 @@ config.libs = [
             Object(Matching, "game/m_fbdemo_triforce.c"),
             Object(Matching, "game/m_fbdemo_wipe1.c"),
             Object(Matching, "game/m_field_assessment.c"),
-            Object(NonMatching, "game/m_field_info.c"),
+            Object(Matching, "game/m_field_info.c"),
             Object(NonMatching, "game/m_field_make.c"),
             Object(NonMatching, "game/m_fishrecord.c"),
             Object(NonMatching, "game/m_flashrom.c"),
