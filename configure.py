f9aae092f481eab62cf2a72255cf7e8b7182b6c6
Cuyler36 <Cuyler36@users.noreply.github.com>
2026-05-07 13:14:59 -0400

    Actually link m_kankyo

--- configure.py
+++ configure.py
@@ -1136,7 +1136,7 @@ config.libs = [
             Object(NonMatching, "game/m_island.c"),
             Object(NonMatching, "game/m_item_name.c"),
             Object(Matching, "game/m_kabu_manager.c"),
-            Object(NonMatching, "game/m_kankyo.c"),
+            Object(Matching, "game/m_kankyo.c"),
             Object(Matching, "game/m_land.c"),
             Object(NonMatching, "game/m_ledit_ovl.c"),
             Object(Matching, "game/m_lib.c"),
