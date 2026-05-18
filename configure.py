a6a2324ce6e44124e9bb76b1a08376b51581d234
Cuyler36 <Cuyler36@users.noreply.github.com>
2026-05-18 02:33:14 -0400

    Link m_string

--- configure.py
+++ configure.py
@@ -1302,7 +1302,7 @@ config.libs = [
             Object(Matching, "game/m_snowman.c"),
             Object(Matching, "game/m_soncho.c"),
             Object(NonMatching, "game/m_start_data_init.c"),
-            Object(NonMatching, "game/m_string.c"),
+            Object(Matching, "game/m_string.c"),
             Object(Matching, "game/m_submenu.c"),
             Object(Matching, "game/m_submenu_ovl.c"),
             Object(NonMatching, "game/m_tag_ovl.c"),
--- src/game/m_string.c
+++ src/game/m_string.c
@@ -6,72 +6,43 @@
 #include "m_font.h"
 #include "m_mail.h"
 
-static u8 buff[MAIL_BODY_LEN - 1] ATTRIBUTE_ALIGN(32); // TODO: proper bss ordering
 static u32 String_rom_start;
-static u32 String_table_rom_start;
 
 extern void mString_aram_init() {
     String_rom_start = JW_GetAramAddress(RESOURCE_STRING);
 }
 
-static void mString_Get_StringDataAddressAndSize(int string_no, u32* string_addr, u32* string_size) {
-    mMsg_Get_BodyParam(String_table_rom_start, String_rom_start, string_no, string_addr, string_size);
-}
-
 extern void mString_Load_StringFromRom(u8* dst, int dst_len, int str_no) {
-    // static u8 buff[MAIL_BODY_LEN - 1] ATTRIBUTE_ALIGN(32); // TODO: proper bss ordering
+    mMsg_bmg_info_c bmg_info;
 
-    u32 string_data_addr;
-    u32 string_size;
-
-    if (str_no >= 0 && str_no < mString_MAX_STR) {
-        mString_Get_StringDataAddressAndSize(str_no, &string_data_addr, &string_size);
-
-        if (string_size == 0) {
-            mem_clear(dst, dst_len, CHAR_SPACE);
-        } else if (string_data_addr != 0) {
-            u32 base_addr = ALIGN_PREV(string_data_addr, 32);
-            u32 ofs = string_data_addr - base_addr;
-            u32 dma_size = ALIGN_NEXT(ofs + string_size, 32);
-            int i;
-            int copy_size;
-            u8* s_dst;
-            u8* s_src;
-
-            _JW_GetResourceAram(base_addr, buff, dma_size);
-            if (dst_len < (int)string_size) {
-                string_size = dst_len;
-            }
-
-            s_src = buff + ofs;
-            s_dst = dst;
-            copy_size = string_size;
-            for (i = 0; i < copy_size; i++) {
-                *s_dst++ = *s_src++;
-            }
-
-            for (i; i < dst_len; i++) {
-                *s_dst++ = CHAR_SPACE;
-            }
-        }
+    mMsgLoad_bmg_init(&bmg_info, String_rom_start, str_no, dst, dst_len);
+    if (mMsgLoad_bmg_load(&bmg_info)) {
+        mem_clear(dst + bmg_info.load_size, dst_len - bmg_info.load_size, CHAR_SPACE);
     }
 }
 
 extern int mString_Load_YearStringFromRom(u8* dst, lbRTC_year_t year) {
+    int len;
+
     if (year < lbRTC_YEAR_MIN || year > lbRTC_YEAR_MAX) {
         year = GAME_YEAR_MIN;
     }
 
-    return mFont_UnintToString(dst, 6, year, 6, TRUE, FALSE, FALSE);
+    len = mFont_UnintToString(dst, mMsg_FREE_STRING_LEN, year, mMsg_FREE_STRING_LEN, TRUE, FALSE, FALSE);
+    mString_Load_StringFromRom(dst + len, mMsg_FREE_STRING_LEN - len, 3);
+    return mMsg_Get_Length_String(dst, mMsg_FREE_STRING_LEN);
 }
 
 extern int mString_Load_MonthStringFromRom(u8* dst, lbRTC_month_t month) {
+    int len;
+
     if (month < lbRTC_JANUARY || month > lbRTC_DECEMBER) {
         month = lbRTC_JANUARY;
     }
 
-    mString_Load_StringFromRom(dst, 9, mString_MONTH_START + month - 1);
-    return mMsg_Get_Length_String(dst, 9);
+    len = mFont_UnintToString(dst, mMsg_FREE_STRING_LEN, month, mMsg_FREE_STRING_LEN, TRUE, FALSE, FALSE);
+    mString_Load_StringFromRom(dst + len, mMsg_FREE_STRING_LEN - len, 4);
+    return mMsg_Get_Length_String(dst, mMsg_FREE_STRING_LEN);
 }
 
 extern int mString_Load_WeekStringFromRom(u8* dst, lbRTC_weekday_t weekday) {
@@ -79,95 +50,70 @@ extern int mString_Load_WeekStringFromRom(u8* dst, lbRTC_weekday_t weekday) {
         weekday = lbRTC_SUNDAY;
     }
 
-    mString_Load_StringFromRom(dst, 9, mString_WEEKDAY_START + weekday);
-    return mMsg_Get_Length_String(dst, 9);
+    mString_Load_StringFromRom(dst, mMsg_FREE_STRING_LEN, mString_WEEKDAY_START + weekday);
+    return mMsg_Get_Length_String(dst, mMsg_FREE_STRING_LEN);
 }
 
 extern int mString_Load_DayStringFromRom(u8* dst, lbRTC_day_t day) {
+    int len;
+
     if (day < 1 || day > 31) {
         day = 1;
     }
 
-    mString_Load_StringFromRom(dst, 4, mString_DAY_START + day - 1);
-    return mMsg_Get_Length_String(dst, 4);
+    len = mFont_UnintToString(dst, mMsg_FREE_STRING_LEN, day, mMsg_FREE_STRING_LEN, TRUE, FALSE, FALSE);
+    mString_Load_StringFromRom(dst + len, mMsg_FREE_STRING_LEN - len, 5);
+    return mMsg_Get_Length_String(dst, mMsg_FREE_STRING_LEN);
 }
 
 extern int mString_Load_HourStringFromRom(u8* dst, lbRTC_hour_t hour) {
-    int hour_num_len;
-    int clamped_hour;
-    int specifier;
+    int len;
+    int num_len;
+    int str_no;
 
     if (hour > 23) {
         hour = 0;
     }
 
-    clamped_hour = hour;
-    switch (clamped_hour) {
-        case 0:
-            clamped_hour = 12;
-            break;
-        case 12:
-            clamped_hour = 12;
-            break;
-
-        default:
-            clamped_hour %= 12;
-            break;
+    if (hour < 12) {
+        str_no = 1;
+    } else {
+        str_no = 2;
     }
 
-    specifier = hour < 12 ? mString_AM : mString_PM;
-    hour_num_len = mFont_UnintToString(dst, 15, clamped_hour, 15, TRUE, FALSE, FALSE);
-    dst[hour_num_len] = CHAR_SPACE;
-    hour_num_len++;
-
-    /* Load AM/PM string */
-    mString_Load_StringFromRom(dst + hour_num_len, 15 - hour_num_len, specifier);
-    return mMsg_Get_Length_String(dst, 15);
-}
-
-extern int mString_Load_HourStringFromRom2(u8* dst, lbRTC_hour_t hour) {
-    int clamped_hour;
-
-    if (hour > 23) {
-        hour = 0;
-    }
-
-    clamped_hour = hour;
-    switch (clamped_hour) {
-        case 0:
-        case 12:
-            clamped_hour = 12;
-            break;
-
-        default:
-            clamped_hour %= 12;
-            break;
-    }
-
-    return mFont_UnintToString(dst, 15, clamped_hour, 2, TRUE, FALSE, FALSE);
+    mString_Load_StringFromRom(dst, mMsg_FREE_STRING_LEN, str_no);
+    len = mMsg_Get_Length_String(dst, mMsg_FREE_STRING_LEN);
+    num_len = mFont_UnintToString(dst + len, mMsg_FREE_STRING_LEN - len, hour % 12, mMsg_FREE_STRING_LEN - len, TRUE, FALSE, FALSE);
+    mString_Load_StringFromRom(dst + (len + num_len), mMsg_FREE_STRING_LEN - (len + num_len), 6);
+    return mMsg_Get_Length_String(dst, mMsg_FREE_STRING_LEN);
 }
 
 extern int mString_Load_MinStringFromRom(u8* dst, lbRTC_min_t min) {
+    int len;
+
     if (min > 59) {
         min = 0;
     }
 
-    return mFont_UnintToString(dst, 4, min, 2, TRUE, TRUE, TRUE);
+    len = mFont_UnintToString(dst, mMsg_FREE_STRING_LEN, min, mMsg_FREE_STRING_LEN, TRUE, FALSE, FALSE);
+    mString_Load_StringFromRom(dst + len, mMsg_FREE_STRING_LEN - len, min % 10 + 0x5DE);
+    return mMsg_Get_Length_String(dst, mMsg_FREE_STRING_LEN);
 }
 
 extern int mString_Load_SecStringFromRom(u8* dst, lbRTC_sec_t sec) {
+    int len;
+
     if (sec > 59) {
         sec = 0;
     }
 
-    return mFont_UnintToString(dst, 5, sec, 2, TRUE, TRUE, TRUE);
+    len = mFont_UnintToString(dst, mMsg_FREE_STRING_LEN, sec, mMsg_FREE_STRING_LEN, TRUE, FALSE, FALSE);
+    mString_Load_StringFromRom(dst + len, mMsg_FREE_STRING_LEN - len, 8);
+    return mMsg_Get_Length_String(dst, mMsg_FREE_STRING_LEN);
 }
 
 extern int mString_Load_NumberStringAddUnitFromRom(u8* dst, u16 num, int str_no) {
-    int num_len = mFont_UnintToString(dst, mMsg_FREE_STRING_LEN, num, mMsg_FREE_STRING_LEN, TRUE, FALSE, TRUE);
-    u8* n_dst = dst + num_len;
-    dst[num_len] = CHAR_SPACE;
-    num_len++;
+    int num_len = mFont_UnintToString(dst, mMsg_FREE_STRING_LEN, num, mMsg_FREE_STRING_LEN, TRUE, FALSE, FALSE);
 
     /* Add the unit */
     mString_Load_StringFromRom(dst + num_len, mMsg_FREE_STRING_LEN - num_len, str_no);
